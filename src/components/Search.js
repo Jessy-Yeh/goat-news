d32071a7dc345f3a14d68ed247dc0a29dd298829
Jessy Yeh <jiejun.yeh@gmail.com>
2022-03-16 18:52:43 +0000

    Add search content and style

--- src/components/Search.js
+++ src/components/Search.js
@@ -1,5 +1,10 @@
 function Search() {
-  return <p>YO</p>;
+  return (
+    <div className="search-container">
+      <input className="search"></input>
+      <img className="search-icon" src="../images/search.png" />
+    </div>
+  );
 }
 
 export default Search;
