f6a02dd3ca08cb9a8d1de4995013023e9b554bcb
Jessy Yeh <jiejun.yeh@gmail.com>
2022-03-16 18:26:23 +0000

    Add content and style to Header

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,5 +1,14 @@
 function Header() {
-  return <p>Hessd</p>;
+  return (
+    <header>
+      <div className="logo-container">
+        <img className="logo-icon" src="../images/logo.png"></img>
+        <h1>Goat News</h1>
+      </div>
+      <p>Jessy Yeh / jiejun.yeh@gmail.com</p>
+      <button>Login</button>
+    </header>
+  );
 }
 
 export default Header;
