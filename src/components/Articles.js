84f730703c863843bad48e8ec128bf6acb13481a
Jessy Yeh <jiejun.yeh@gmail.com>
2022-03-17 09:37:13 +0000

    Add content and style to Articles

--- src/components/Articles.js
+++ src/components/Articles.js
@@ -1,5 +1,47 @@
 function Articles() {
-  return <p>Articles</p>;
+  return (
+    <div className="articles-container">
+      <div className="article">
+        <img src="https://i0.wp.com/9to5google.com/wp-content/uploads/sites/4/2021/08/samsung_s_pen_fold_3_case_8.jpg?resize=1200%2C628&quality=82&strip=all&ssl=1" />
+        <div className="article-content">
+          <h2>Galaxy Z Fold 3 comes bundled with an S Pen Case</h2>
+          <p>
+            Samsung's Galaxy Z Fold 3 currently comes bundled with an S Pen case
+            at a new 2022 low of $349 off, plus the OG Google Pixel Stand, and
+            more.
+          </p>
+        </div>
+        <p className="source-name">9to5Google</p>
+      </div>
+
+      <div className="article">
+        <img src="https://i0.wp.com/9to5google.com/wp-content/uploads/sites/4/2021/08/samsung_s_pen_fold_3_case_8.jpg?resize=1200%2C628&quality=82&strip=all&ssl=1" />
+        <h2>Galaxy Z Fold 3 comes bundled with an S Pen Case</h2>
+        <p className="source-name">9to5Google</p>
+      </div>
+
+      <div className="article">
+        <img src="https://i0.wp.com/9to5google.com/wp-content/uploads/sites/4/2021/08/samsung_s_pen_fold_3_case_8.jpg?resize=1200%2C628&quality=82&strip=all&ssl=1" />
+        <h2>Galaxy Z Fold 3 comes bundled with an S Pen Case</h2>
+        <p className="source-name">9to5Google</p>
+      </div>
+      <div className="article">
+        <img src="https://i0.wp.com/9to5google.com/wp-content/uploads/sites/4/2021/08/samsung_s_pen_fold_3_case_8.jpg?resize=1200%2C628&quality=82&strip=all&ssl=1" />
+        <h2>Galaxy Z Fold 3 comes bundled with an S Pen Case</h2>
+        <p className="source-name">9to5Google</p>
+      </div>
+      <div className="article">
+        <img src="https://i0.wp.com/9to5google.com/wp-content/uploads/sites/4/2021/08/samsung_s_pen_fold_3_case_8.jpg?resize=1200%2C628&quality=82&strip=all&ssl=1" />
+        <h2>Galaxy Z Fold 3 comes bundled with an S Pen Case</h2>
+        <p className="source-name">9to5Google</p>
+      </div>
+      <div className="article">
+        <img src="https://i0.wp.com/9to5google.com/wp-content/uploads/sites/4/2021/08/samsung_s_pen_fold_3_case_8.jpg?resize=1200%2C628&quality=82&strip=all&ssl=1" />
+        <h2>Galaxy Z Fold 3 comes bundled with an S Pen Case</h2>
+        <p className="source-name">9to5Google</p>
+      </div>
+    </div>
+  );
 }
 
 export default Articles;
