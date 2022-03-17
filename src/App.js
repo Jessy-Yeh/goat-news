940d2cb88fc27e5d271aae99b73d37a297cdcf4c
Jessy Yeh <jiejun.yeh@gmail.com>
2022-03-17 15:53:20 +0000

    Add params to API

--- src/App.js
+++ src/App.js
@@ -10,7 +10,12 @@ function App() {
   useEffect(() => {
     axios
       .get(
-        `https://gnews.io/api/v4/search?q=example&token=790aa44ab9e0476cbc2481efa3b06ad9&lang=en`
+        `https://gnews.io/api/v4/search?q=example&token=790aa44ab9e0476cbc2481efa3b06ad9&lang=en`,
+        {
+          params: {
+            max: 10,
+          },
+        }
       )
       .then((response) => {
         const articleArray = response.data.articles;
