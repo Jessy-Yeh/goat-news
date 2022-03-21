b44a027db58e51686321f7a360766ab8ec98ab9e
Jessy Yeh <jiejun.yeh@gmail.com>
2022-03-21 12:23:22 +0000

    Add placeholder

--- src/components/Search.js
+++ src/components/Search.js
@@ -7,7 +7,11 @@ function Search({ setQuery }) {
 
   return (
     <div className="search-container">
-      <input className="search" onKeyUp={searchItems}></input>
+      <input
+        className="search"
+        placeholder="search something here..."
+        onKeyUp={searchItems}
+      ></input>
       <div className="search-shadow"></div>
       <img className="search-icon" src="assets/search.png" />
     </div>
